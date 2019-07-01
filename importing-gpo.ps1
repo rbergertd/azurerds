8b8a83ec3a9b0e745121f4c97d4185f2eeb8f131
Ryan Berger <ryan.berger@techdata.com>
2019-07-01 17:04:58 -0400

    Update importing-gpo.ps1

--- importing-gpo.ps1
+++ importing-gpo.ps1
@@ -16,5 +16,5 @@ New-ADOrganizationalUnit $RDSOU
 #Create Security Group, GPO, import policy backup, and link to Domain root.
 new-adgroup -Name "RDS Users" -SamAccountName RDSUsers -GroupCategory Security -GroupScope Global -DisplayName "RDS Users" -Description "RDS Users"
 new-gpo -name "ConfigureRDSSessionHosts" -Comment "Configure the appropriate GPO's for a default session hosts configuration."
-import-gpo -backupid 48A0F1BA-62CA-4C81-AAA7-2AE77980C956 -TargetName "ConfigureRDSSessionHosts" -Path C:\GPOBackup
+import-gpo -backupid 71D7905A-246C-40DA-9DBC-85293A13D5E9 -TargetName "ConfigureRDSSessionHosts" -Path C:\GPOBackup
 new-gplink -name "ConfigureRDSSessionHosts" -Enforced Yes -LinkEnabled Yes -Target $GPLinkTarget
